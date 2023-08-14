72fd3c68ed64307afda96396a2af6f2944d92824
unknown <ehadzic4@etf.unsa.ba>
2023-08-14 23:05:05 +0200

    deleted the folder for hierarchy changes

--- auction-app/src/pages/MyAccount.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Styles.css";
-
-const MyAccount = () => {
-  return <div className="div-placeholder-style">My Account Page Content</div>;
-};
-
-export default MyAccount;
