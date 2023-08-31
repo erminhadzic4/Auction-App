271f741c6b9b38089447302bc6abbef29095098f
unknown <ehadzic4@etf.unsa.ba>
2023-08-31 15:21:05 +0200

    merge conflicts

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -52,41 +52,6 @@ const ProductDetails = () => {
     setBidAmount(event.target.value);
   };
 
-  const formatTimeLeft = (endingTime) => {
-    const now = new Date();
-    const endDate = new Date(endingTime);
-    const timeDifference = endDate - now;
-
-    if (timeDifference <= 0) {
-      return "Expired";
-    }
-
-    const weeks = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 7));
-    const days = Math.floor(
-      (timeDifference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24)
-    );
-    const hours = Math.floor(
-      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor(
-      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
-    );
-
-    if (weeks > 0 && days > 0) {
-      return `${weeks} week${weeks !== 1 ? "s" : ""} ${days} day${
-        days !== 1 ? "s" : ""
-      }`;
-    } else if (weeks > 0) {
-      return `${weeks} week${weeks !== 1 ? "s" : ""}`;
-    } else if (days > 0) {
-      return `${days} day${days !== 1 ? "s" : ""}`;
-    } else {
-      return `${hours} hour${hours !== 1 ? "s" : ""} ${minutes} minute${
-        minutes !== 1 ? "s" : ""
-      }`;
-    }
-  };
-
   const handlePlaceBid = () => {
     if (
       !localStorage.getItem("isAuth") ||
